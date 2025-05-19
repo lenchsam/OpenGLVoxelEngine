6c500bf944ffeba99ada80e33f9d7d0f48a1ea37
Sam <114984800+lenchsam@users.noreply.github.com>
2025-05-19 16:58:04 +0100

    different coloured vertex
    still following https://learnopengl.com/

--- OpenGLVoxelEngine/OpenGLVoxelEngine/Application.cpp
+++ OpenGLVoxelEngine/OpenGLVoxelEngine/Application.cpp
@@ -55,8 +55,8 @@ int Application::run() {
     float vertices[] = {
         // positions         // colors
          0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+        -0.5f, -0.5f, 0.0f,  1.0f, 1.0f, 0.0f,  // bottom left
+         0.0f,  0.5f, 0.0f,  1.0f, 0.0f, 1.0f   // top 
     };
 
     unsigned int VBO, VAO;
--- OpenGLVoxelEngine/OpenGLVoxelEngine/shader.frag
+++ OpenGLVoxelEngine/OpenGLVoxelEngine/shader.frag
@@ -1,9 +1,8 @@
 #version 460 core
-out vec4 FragColor;
+out vec4 FragColor;  
+in vec3 ourColor;
   
-uniform vec4 vertexColor;
-
 void main()
 {
-    FragColor = vertexColor;
-} 
+    FragColor = vec4(ourColor, 1.0);
+}
